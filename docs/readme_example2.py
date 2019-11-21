055fa2ada36e84d1a31a6ac82ace639eb211a52b
Fabiano Engler Neto <fabianoengler@gmail.com>
2019-11-20 22:36:48 -0300

    Add README and examples

---
+++ docs/readme_example2.py
@@ -0,0 +1,22 @@
+
+from dictvars import dictvars, varsnamed
+
+def somefunc():
+    a = '1'
+    b = '2'
+    c = '3'
+    leak = b
+    return dictvars(a, b)
+
+print(somefunc())
+
+
+
+def somefunc2():
+    a = '1'
+    b = '2'
+    c = '3'
+    no_leaks_now = b
+    return varsnamed('a', 'b')
+
+print(somefunc2())
